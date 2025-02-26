a9347f19ab837c8b345d20a9607472bac38b90fa
nht071203 <157948189+nht071203@users.noreply.github.com>
2025-02-27 00:34:42 +0700

    Update Order.java

--- ProjectPRM392/app/src/main/java/com/example/projectprm392/Order.java
+++ ProjectPRM392/app/src/main/java/com/example/projectprm392/Order.java
@@ -1,4 +1,51 @@
 package com.example.projectprm392;
 
+import java.util.List;
+
 public class Order {
+    private String orderId;
+    private String orderDate;
+    private List<OrderDetail> orderDetails;
+
+    public Order() {
+    }
+
+    public Order(String orderId, String orderDate, List<OrderDetail> orderDetails) {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+        this.orderDetails = orderDetails;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public List<OrderDetail> getOrderDetails() {
+        return orderDetails;
+    }
+
+    public void setOrderDetails(List<OrderDetail> orderDetails) {
+        this.orderDetails = orderDetails;
+    }
+
+    // Tính tổng giá của toàn bộ order
+    public double getTotalOrderPrice() {
+        double total = 0;
+        for (OrderDetail detail : orderDetails) {
+            total += detail.getTotalPrice();
+        }
+        return total;
+    }
 }
