a007bb2fba7db21c5fce6d0ed6a4164e62696493
Tran Phuong Nam <namtpce170126@fpt.edu.vn>
2025-02-28 21:12:33 +0700

    UI Search

---
+++ ProjectPRM392/app/src/main/java/com/example/projectprm392/SearchControl/SearchProduct.java
@@ -0,0 +1,58 @@
+package com.example.projectprm392.SearchControl;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.SearchView;
+
+import com.example.projectprm392.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SearchProduct#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SearchProduct extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public SearchProduct() {
+
+    }
+
+    public static SearchProduct newInstance(String param1, String param2) {
+        SearchProduct fragment = new SearchProduct();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_search_product, container, false);
+
+
+        return view;
+    }
+}
