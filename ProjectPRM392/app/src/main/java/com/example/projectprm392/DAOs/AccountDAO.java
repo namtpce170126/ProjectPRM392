ef121c19871968a64a2b0be6db55175d2410c16b
thucnhce171748 <thucnhce171748@fpt.edu.vn>
2025-03-05 21:31:52 +0700

    add BaseDAO

--- ProjectPRM392/app/src/main/java/com/example/projectprm392/DAOs/AccountDAO.java
+++ ProjectPRM392/app/src/main/java/com/example/projectprm392/DAOs/AccountDAO.java
@@ -1,7 +1,6 @@
 package com.example.projectprm392.DAOs;
 
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 
 import com.example.projectprm392.Database.DatabaseHelper;
 import com.example.projectprm392.Models.Account;
@@ -9,51 +8,35 @@ import com.example.projectprm392.Models.Account;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AccountDAO {
-    private SQLiteDatabase db;
-    private DatabaseHelper dbHelper;
-
+public class AccountDAO extends SingletonBaseDAO {
     public AccountDAO(DatabaseHelper dbHelper) {
-        this.dbHelper = dbHelper;
+        super(dbHelper);
     }
 
-    // Mở kết nối DB
-    public void open() {
-        db = dbHelper.getWritableDatabase();
-    }
-
-    // Đóng kết nối DB
-    public void close() {
-        dbHelper.close();
-    }
-
-    // READ - Lấy danh sách tất cả tài khoản
+    // Lấy danh sách tất cả tài khoản
     public List<Account> getAllAccounts() {
+        open();
         List<Account> accounts = new ArrayList<>();
         Cursor cursor = db.rawQuery("SELECT * FROM account WHERE isDelete = 0", null);
         if (cursor.moveToFirst()) {
             do {
                 Account account = new Account(
-                        cursor.getInt(0), // acc_id
-                        cursor.getInt(1), // role_id
-                        cursor.getString(2), // username
-                        cursor.getString(3), // password
-                        cursor.getString(4), // fullname
-                        cursor.getString(5), // phone_number
-                        cursor.getString(6), // email
-                        cursor.getString(7), // birthday
-                        cursor.getString(8), // address
-                        cursor.getInt(9) // isDelete
+                        cursor.getInt(0), cursor.getInt(1), cursor.getString(2),
+                        cursor.getString(3), cursor.getString(4), cursor.getString(5),
+                        cursor.getString(6), cursor.getString(7), cursor.getString(8),
+                        cursor.getInt(9)
                 );
                 accounts.add(account);
             } while (cursor.moveToNext());
         }
         cursor.close();
+        close();
         return accounts;
     }
 
     // Lấy tài khoản theo ID
     public Account getAccountById(int accId) {
+        open();
         Cursor cursor = db.rawQuery("SELECT * FROM account WHERE acc_id = ?", new String[]{String.valueOf(accId)});
         if (cursor.moveToFirst()) {
             Account account = new Account(
@@ -63,9 +46,11 @@ public class AccountDAO {
                     cursor.getInt(9)
             );
             cursor.close();
+            close();
             return account;
         }
         cursor.close();
+        close();
         return null;
     }
 
