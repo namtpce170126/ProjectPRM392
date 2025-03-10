8c647d8c17cdd7e1161ea7260e6704a7fa54b507
kiri6609 <kdang6609@gmail.com>
2025-03-10 11:30:04 +0700

    optimized 1st

--- ProjectPRM392/app/src/main/java/com/example/projectprm392/PermissionControl/RegisterActivity.java
+++ ProjectPRM392/app/src/main/java/com/example/projectprm392/PermissionControl/RegisterActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,15 +19,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.projectprm392.DAOs.AccountDAO;
+import com.example.projectprm392.Database.DatabaseHelper;
 import com.example.projectprm392.MainActivity;
+import com.example.projectprm392.Models.Account;
 import com.example.projectprm392.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private ImageView btnClose;
+    ImageView btnClose;
     TextView linkLogin;
     EditText etPhoneNumber;
     Button btnRegister;
+    CheckBox chkTerms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +43,7 @@ public class RegisterActivity extends AppCompatActivity {
         linkLogin = findViewById(R.id.tvLogin);
         etPhoneNumber = findViewById(R.id.etPhone);
         btnRegister = findViewById(R.id.btnContinue);
+        chkTerms = findViewById(R.id.chkTerms);
 
         btnClose.setOnClickListener(view -> {
             Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
@@ -54,20 +60,30 @@ public class RegisterActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                validatePhoneNumber(s.toString());
+                validatePhoneNumber();
             }
 
             @Override
             public void afterTextChanged(Editable s) {}
         });
 
+        // Kiểm tra checkbox khi thay đổi trạng thái
+        chkTerms.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            validatePhoneNumber();
+        });
+
         // Nút "Tiếp tục"
         btnRegister.setOnClickListener(v -> {
             String phoneNumber = etPhoneNumber.getText().toString();
-            if (validatePhoneNumber(phoneNumber)) {
+            if (validatePhoneNumber()) {
+                // Kiểm tra số điện thoại đã tồn tại chưa
+                if (isPhoneNumberExists(phoneNumber)) {
+                    Toast.makeText(RegisterActivity.this, "Số điện thoại đã được đăng ký!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
                 // Thực thi chuyển dữ liệu số điện thoại sang fragment khác
                 savePhoneNumber(phoneNumber); // Lưu số điện thoại vào SharedPreferences
-                Toast.makeText(RegisterActivity.this, "Số điện thoại đã lưu!", Toast.LENGTH_SHORT).show();
 
                 // Chuyển sang Permission OTPFragment
                 Intent intent = new Intent(RegisterActivity.this, Permisson.class);
@@ -86,15 +102,19 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     // Hàm kiểm tra số điện thoại
-    private boolean validatePhoneNumber(String phone) {
-        if (phone.length() != 10 || !phone.matches("^0[0-9]{9}$")) {
-            btnRegister.setEnabled(false);
-            btnRegister.setBackgroundResource(R.drawable.bg_button_disable); // Cập nhật background
-            return false;
-        } else {
+    private boolean validatePhoneNumber() {
+        String phone = etPhoneNumber.getText().toString();
+        boolean isPhoneValid = phone.length() == 10 && phone.matches("^0[0-9]{9}$");
+        boolean isTermsChecked = chkTerms.isChecked();
+
+        if (isPhoneValid && isTermsChecked) {
             btnRegister.setEnabled(true);
             btnRegister.setBackgroundResource(R.drawable.bg_button_enable);
             return true;
+        } else {
+            btnRegister.setEnabled(false);
+            btnRegister.setBackgroundResource(R.drawable.bg_button_disable); // Cập nhật background
+            return false;
         }
     }
 
@@ -104,5 +124,13 @@ public class RegisterActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString("phone_number", phoneNumber);
         editor.apply();
+    }
+
+    // Kiểm tra sđt đã có chưa
+    private boolean isPhoneNumberExists(String phoneNumber) {
+        DatabaseHelper dbHelper = new DatabaseHelper(RegisterActivity.this);
+        AccountDAO accountDAO = new AccountDAO(dbHelper);
+        Account account = accountDAO.getAccountByPhone(phoneNumber);
+        return account != null; // Nếu tài khoản tồn tại, trả về true
     }
 }
